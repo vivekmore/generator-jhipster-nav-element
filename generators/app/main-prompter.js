d3232b1538a08dbf152361f95163934a28c54c6d
Vivek <vivz.thats.me@gmail.com>
2018-07-04 20:49:27 -0400

    lint main-prompter.js

--- generators/app/main-prompter.js
+++ generators/app/main-prompter.js
@@ -1,49 +1,46 @@
-'use strict';
-
-var chalk = require('chalk'),
-  constants = require('./constants'),
-  defaultTemplatePrompter = require('./about-us/about-us-template-prompter'),
-  defaultNg2TemplatePrompter = require('./about-us/about-us-ng2-template-prompter');
+const chalk = require('chalk');
+const constants = require('./constants');
+const defaultTemplatePrompter = require('./about-us/about-us-template-prompter');
+const defaultNg2TemplatePrompter = require('./about-us/about-us-ng2-template-prompter');
 
 module.exports = {
-  promptToChooseATemplate,
-  promptTemplateSpecificQuestions
+    promptToChooseATemplate,
+    promptTemplateSpecificQuestions
 };
 
 function promptToChooseATemplate(generator) {
+    const done = generator.async();
 
-  var done = generator.async();
+    generator.prompt({
+        type: 'list',
+        name: 'templateType',
+        message: 'Which *type* of page would you like to generate? (More templates will be added soon! Stay tuned...)',
+        choices: [
+            {
+                name: 'About Us',
+                value: constants.TEMPLATE_TYPE.DEFAULT
+            }
+        ]
 
-  generator.prompt({
-    type: 'list',
-    name: 'templateType',
-    message: 'Which *type* of page would you like to generate? (More templates will be added soon! Stay tuned...)',
-    choices: [
-      {
-        name: 'About Us',
-        value: constants.TEMPLATE_TYPE.DEFAULT
-      }
-    ]
-
-  }).then(function (prompt) {
-    generator.props.templateType = prompt.templateType;
-    // To access props later use this.props.someOption;
-    done();
-  });
+    }).then((prompt) => {
+        generator.props.templateType = prompt.templateType;
+        // To access props later use this.props.someOption;
+        done();
+    });
 }
 
 function promptTemplateSpecificQuestions(generator) {
-  switch (generator.props.templateType) {
+    switch (generator.props.templateType) {
     case constants.TEMPLATE_TYPE.DEFAULT:
-      askDefaultTemplateQuestions(generator);
-      break;
-  }
+        askDefaultTemplateQuestions(generator);
+        break;
+    }
 }
 
 function askDefaultTemplateQuestions(generator) {
-  if (generator.jhipsterAppConfig.clientFramework === 'angularX') {
-    defaultNg2TemplatePrompter.askQuestions(generator);
-  } else {
-    defaultTemplatePrompter.askQuestions(generator);
-  }
+    if (generator.jhipsterAppConfig.clientFramework === 'angularX') {
+        defaultNg2TemplatePrompter.askQuestions(generator);
+    } else {
+        defaultTemplatePrompter.askQuestions(generator);
+    }
 }
