4ab8c4a78982dc7ce65221a6a06204395469061e
Vivek <vivz.thats.me@gmail.com>
2018-07-07 21:11:49 -0400

    remove angularjs code

--- generators/app/main-writer.js
+++ generators/app/main-writer.js
@@ -1,5 +1,4 @@
 const constants = require('./constants');
-const defaultTemplateWriter = require('./about-us/about-us-template-writer');
 const defaultNg2TemplateWriter = require('./about-us/about-us-ng2-template-writer');
 
 module.exports = {
@@ -19,7 +18,5 @@ function writeTemplate(generator) {
 function writeDefaultTemplate(generator) {
     if (generator.jhipsterAppConfig.clientFramework === 'angularX') {
         defaultNg2TemplateWriter.write(generator);
-    } else {
-        defaultTemplateWriter.write(generator);
     }
 }
