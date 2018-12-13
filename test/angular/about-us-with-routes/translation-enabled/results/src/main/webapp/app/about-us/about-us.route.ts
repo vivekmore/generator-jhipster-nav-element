6f4442e4adccc5eb5c461aa0391c327858bd2a2d
Vivek <vivz.thats.me@gmail.com>
2018-12-13 09:04:25 -0500

    [nested-routes] add tests/expectations for the new template output

---
+++ test/angular/about-us-with-routes/translation-enabled/results/src/main/webapp/app/about-us/about-us.route.ts
@@ -0,0 +1,18 @@
+import { Route } from '@angular/router';
+
+import { UserRouteAccessService } from 'app/core';
+import { AboutUsComponent } from './';
+import { PAGE_ONE_ROUTE } from './page-one/page-one.route';
+import { PAGE_TWO_ROUTE } from './page-two/page-two.route';
+
+export const ABOUT_US_ROUTE: Route = {
+  path: 'about-us',
+  component: AboutUsComponent,
+  data: {
+    authorities: [],
+    pageTitle: 'about-us.title'
+  },
+  canActivate: [UserRouteAccessService],
+  children: []
+};
+.
---
+++ test/angular/about-us-with-routes/translation-enabled/results/src/test/javascript/spec/app/about-us/page-two/page-two.component.spec.ts
@@ -0,0 +1,3 @@
+describe('PageTwoComponent Tests', () => {
+
+});
