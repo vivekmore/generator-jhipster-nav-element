0a029ef9898d8c35dbd155d8faabc43256c7b7f4
Vivek 🧐 <vivekmore@noreply.users.github.com>
2018-12-29 18:06:15 -0500

    add tests for nested-routes when useSass is true

---
+++ test/angular/nested-routes/use-sass/results/src/main/webapp/app/about-us/about-us.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'test-about-us',
+  templateUrl: './about-us.component.html',
+  styleUrls: [
+    'about-us.component.scss'
+  ]
+})
+export class AboutUsComponent implements OnInit {
+
+  message: string;
+
+  constructor() {
+    this.message = 'AboutUsComponent message';
+  }
+
+  ngOnInit() {
+  }
+
+}
