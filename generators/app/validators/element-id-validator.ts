3040fac887c0b7df1642c7e515347869e1510f0f
Vivek 🧐 <vivekmore@noreply.users.github.com>
2020-02-16 06:45:35 -0500

    [typescript] rename js -> ts

--- generators/app/validators/element-id-validator.ts
+++ generators/app/validators/element-id-validator.ts
@@ -1,21 +1,16 @@
-module.exports = {
-    validate,
-    validateTitle
-};
-
 /**
  * Validate the input element-id
  *
  * @param {string} input - input to validate
  */
-function validate(input) {
+export function validate(input) {
     if (/^([a-zA-Z0-9_]*)$/.test(input)) {
         return true;
     }
     return 'Navigation element may contain only alphanumeric values and underscores';
 }
 
-function validateTitle(input) {
+export function validateTitle(input) {
     if (/^([a-zA-Z0-9 ]*)$/.test(input)) {
         return true;
     }
