139f1a6b210fa3612c1a84f77d38ab04cafa2b73
Vivek 🧐 <vivekmore@noreply.users.github.com>
2021-03-28 23:36:22 -0400

    ✅ fix UserRouteAccessService path

--- test/angular/about-us/translation-disabled/results/src/main/webapp/app/about-us/about-us.route.ts
+++ test/angular/about-us/translation-disabled/results/src/main/webapp/app/about-us/about-us.route.ts
@@ -1,6 +1,6 @@
 import { Route } from '@angular/router';
 
-import { UserRouteAccessService } from 'app/core/auth/user-route-access-service';
+import { UserRouteAccessService } from 'app/core/auth/user-route-access.service';
 import { AboutUsComponent } from './about-us.component';
 
 export const ABOUT_US_ROUTE: Route = {
